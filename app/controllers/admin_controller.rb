d312049862f6aad669a14e1744e3e96fadd05a03
Angela Castagno <oesed16@gmail.com>
2018-12-07 20:37:28 -0500

    Estadísticas Admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,6 @@
 class AdminController < ApplicationController
-
-  # Si se desean vistas con JavaScript y CSS diferentes.
+  before_action :authenticate_user!
+  before_action :is_admin?
 
   # layout "admin" Éste layout afecta a todas las vistas.
   layout "admin" #, only[:index, :delete] # O también except, a todas menos a las mencionadas
@@ -8,5 +8,17 @@ class AdminController < ApplicationController
 
   def index
     # render layout: "admin" Sólo afecta ésta vista
+    @users = User.all.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
+    @comments = Comment.all.count
+    @subscriptors = Subscriptor.all.order(created_at: :desc)
+  end
+
+  private
+  def is_admin?
+    unless current_user.admin?
+      flash[:alert] = "You don't have permissions"
+      redirect_to root_path
+    end
   end
 end
