34e41178a6ee61465195e40cac6d75ba56f8c3a0
Angela Castagno <oesed16@gmail.com>
2018-12-17 20:56:53 -0500

    Creación de API de subscriptors

--- app/controllers/subscriptors_controller.rb
+++ app/controllers/subscriptors_controller.rb
@@ -1,8 +1,18 @@
 class SubscriptorsController < ApplicationController
-  
-  layout "signin"
+  protect_from_forgery with: :null_session
+  #  before_action :authenticate_user! Con ésta línea se requiere autenticación y no cualquiera puede acceder a la API.
+  before_action :find_subscriptor, only: [:show, :edit, :update, :destroy]
+  layout "signin",  only: [:new, :create]
 
   def index
+    @subscriptors = Subscriptor.all.order(created_at: :desc)
+    
+
+    respond_to do |format| # Condiciona el formato con el cual se desea parsear los datos.
+      format.html
+      format.json { render json: @subscriptors }
+    end
+
   end
 
   def new
@@ -11,41 +21,65 @@ class SubscriptorsController < ApplicationController
 
   def create
     @subscriptor = Subscriptor.new(subscriptor_params)
-      if @subscriptor.save
+      respond_to do |format|
+        if @subscriptor.save
 
-        # send subscriptor mail
-        UserNotifierMailer.send_subscribe_email(@subscriptor).deliver_now
+          # send subscriptor mail
+          UserNotifierMailer.send_subscribe_email(@subscriptor).deliver_now
 
-        redirect_to posts_path, notice: "Subscription created successfully"
-      else
-        flash[:alert] = "The subscription failed, try again"
-        render :new 
+          format.html { redirect_to posts_path, notice: "Subscription created successfully" }
+          format.json { render :show, status: :created }
+          
+        else
+          format.html {render :new, notice: "The subscription failed, try again"}
+          format.json { render json: @subscriptor.errors, status: :unprocessable_entity }
+          
+        end
       end
+      
   end
 
   def show
-
+    respond_to do |format|
+      format.html
+      format.json { render json: @subscriptor }
+    end
   end
 
   def edit
-
+    
   end
 
   def destroy
-    @subscriptor = Subscriptor.find(params[:id])
     @subscriptor.destroy
-    redirect_to subscriptors_path
+    respond_to do |format|
+      format.html { redirect_to subscriptors_path, notice: "Subscriptor was successfull destroyed"}
+      format.json { head :no_content }
+    end
+    
   end
 
   def update
-    @subscriptor = Subscriptor.find(params[:id])
-    redirect_to subscriptor_path(@subscriptor)
+    respond_to do |format|
+      if @subscriptor.update(subscriptor_params)
+        format.html { redirect_to subscriptor_path(@subscriptor)}
+        format.json { render :show, status: :ok}
+      else
+        format.html { render :edit}
+        format.json { render json: @subscriptor.errors, status: :unprocessable_entity }
+      end
+    end
+    
   end
 
   private
     
   def subscriptor_params # Éste método se conoce como Strong parameters
       params.require(:subscriptor).permit(:name, :email)
+  end
+
+  def find_subscriptor
+    @subscriptor = Subscriptor.find(params[:id])
   end
 
 end
