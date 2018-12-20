3488a11ff0e85837f11e9a9b41ee1a0248e54eaf
Angela Castagno <oesed16@gmail.com>
2018-12-19 20:35:20 -0500

    Delete products y vista /cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -12,6 +12,11 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
+    @order = current_order
+    @item = @order.order_items.find(params[:id])
+    @item.destroy
+    @order.save
+    redirect_to cart_path
   end
 
   private
